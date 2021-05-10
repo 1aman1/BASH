baad22b8f6d6e78d6b1e9f866b69f8a9b26288a2
/---Aman---/ <aman.it1pro@gmail.com>
2021-05-10 15:57:08 +0530

    Update reader.sh

--- reader.sh
+++ reader.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 sourceFile1=$1
-sourceFile2=$1
+sourceFile2=$2
 
-notepad=$2
+notepad=$3
 
-while read line; do
-# reading each line
-if [ $line == $line ]
-	then echo $line >> notepad.txt
-fi
-done < $sourceFile1
+while read cline
+do
+        while read dline
+        do
+                if [[ $cline == $dline ]]
+                then
+                        echo $cline
+                fi
+        done < $sourceFile1
+done < $sourceFile2
