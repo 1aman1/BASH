2a7d06c21fc3b10df38e773c79ec9bf247e288f0
/---Aman---/ <amangupta@outlook.com>
2022-01-25 20:01:01 +0530

    Update Install.sh

--- CassandraDB/Setup/Install.sh
+++ CassandraDB/Setup/Install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# it requires java
+yum install -y java-1.8.0
+java -version
+
 cat << EOF > /etc/yum.repos.d/cassandra.repo
 [cassandra]
 name=Apache Cassandra
@@ -8,7 +13,33 @@ repo_gpgcheck=1
 gpgkey=https://downloads.apache.org/cassandra/KEYS
 EOF
 
-yum install -y  cassandra
-service cassandra start
-service cassandra status
-chkconfig cassandra on
+systemctl daemon-reload
+yum install -y cassandra
+
+# check if it installed properly
+rpm -qi cassandra
+
+cat << EOF > /etc/systemd/system/cassandra.service
+[Unit]
+Description=Apache Cassandra
+After=network.target
+
+[Service]
+PIDFile=/var/run/cassandra/cassandra.pid
+User=cassandra
+Group=cassandra
+ExecStart=/usr/sbin/cassandra -f -p /var/run/cassandra/cassandra.pid
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl start cassandra
+systemctl enable cassandra
+systemctl status cassandra
+
+# if cqlsh throws errors of python library uncomment these
+#yum install -y pip
+#pip install cqlsh
